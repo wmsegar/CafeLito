79dc3f49d79ae83747fa64e64aa765bc26ea65fd
wmsegar <wayne.segar@gmail.com>
2016-06-27 19:17:31 -0400

    Added new getGoogleNearest Method for switching over the the Google Places API to get Nearest CoffeeShops

--- src/main/java/com/mechanitis/demo/coffee/CoffeeShopResource.java
+++ src/main/java/com/mechanitis/demo/coffee/CoffeeShopResource.java
@@ -7,8 +7,14 @@ import org.mongodb.morphia.Morphia;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
 import java.net.URI;
-
+import java.net.URL;
+import java.net.URLConnection;
 
 
 @Path("/coffeeshop")
@@ -28,6 +34,36 @@ public class CoffeeShopResource {
                         .field("location")
                         .near(longitude, latitude, true)
                         .get();
+    }
+
+    //Path to new Service Using Google Places API
+    @Path("gnearest/{latitude}/{longitude}")
+    @GET
+    public String getGoogleNearest(@PathParam("latitude") double latitude ,
+                             @PathParam("longitude") double longitude ) throws IOException {
+
+        final String GOOGLE_API_KEY = "AIzaSyBWhQLxfNxyiQr1kqF2Bo-A0Dg3ZqutgBw";
+        String endpoint = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + latitude + "," + longitude + "&radius=500&name=coffee&key=" + GOOGLE_API_KEY;
+
+        try {
+            URL url = new URL(endpoint);
+
+            URLConnection connection = url.openConnection();
+            InputStream inputStream = connection.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            StringBuilder result = new StringBuilder();
+            String line;
+            while((line = reader.readLine()) != null){
+                result.append(line);
+            }
+            return result.toString();
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+        return null;
     }
 
     @Path("order")
